1c8a05dfbcc2dbe1e2d01732bf88b8f010d2ff2c
adisharma9120 <aditya91sharma73619@gmail.com>
2025-08-21 15:38:28 +0530

    how to make proper structure in rust rather than define differently bcz we want a proper struccture code

---
+++ src/bin/Struct.rs
@@ -0,0 +1,22 @@
+struct  User {
+    name: String,
+    age : u32,
+   
+}
+
+ fn main(){
+    let name = String::from("aditya");
+    let user  = User{
+        name: name,
+        age: 32,
+        
+    };
+    println!("{} is {} year old.", user.name, user.age)
+ }
+
+
+
+
+
+
+
--- src/bin/owener_stack_scop.rs
+++ src/bin/owener_stack_scop.rs
@@ -1,4 +1,4 @@
-// /* 
+// /*
 // # Example = 1
 // Passimng stack variable onsode function */
 // fn  main(){
@@ -11,17 +11,14 @@
 // Jab tu sum(x, y) call karta hai, tab x ka value copy hokar a me chala jata hai aur y ka value copy hokar b me chala jata hai.
 // Isko hum argument pass karna kehte hain.
 // Yaani function ke andar ka naam (a, b) alag hai aur bahar se jo values di jaati hain (x, y) alag hain. */
-
 // fn sum(a: i32, b: i32) -> i32{
 //     let c = a+b;
 //     return c;
 //  }
 
-
-// /* 
+// /*
 //  #Example = 2;
 //  Scoping variable in the same function; */
-
 //  fn main1 (){
 //     let x = 3; // create on stack
 //     {
@@ -38,32 +35,23 @@
 //     println!("Outside block y = {}", y);   // ❌ Error, kyunki y destroy ho gaya
 // } */
 
-
-
-
 // fn main()
 // {
 //     let my_string= String::from("hello");
 //     takes_ownership(my_string.clone());
-//     println!("{}", my_string);  // this  line would cause a complier error beacise ownership has been 
+//     println!("{}", my_string);  // this  line would cause a complier error beacise ownership has been
 //     // moved
 // }
 
 //  fn takes_ownership(some_string: String) {
 //     println!("{}", some_string);
-//      // return ownership 
+//      // return ownership
 // }
 
+fn main() {
+    let s1 = String::from("chal be chutiye ");
+    let s2 = &s1;
 
-
-// fn  main()
-// {
-//     let s1 = String::from("chal be chutiye ");
-//     let s2 = &s1;
-     
-//      println!("{}",s2);
-//      println!("{}",s1) // this is  valid ,The first pointer wasn't invalidated\
-
-// }
-
-//
+    println!("{}", s2);
+    println!("{}", s1) // this is  valid ,The first pointer wasn't invalidated\
+}
