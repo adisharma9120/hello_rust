2e492d8de8f2b5f6ed9b4a3014f2b77a524b4b9d
adisharma9120 <aditya91sharma73619@gmail.com>
2025-08-24 00:33:59 +0530

    simple error handling

---
+++ src/bin/error_handling.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let numerator = 10;
+    let denominator = 0;
+
+    let result = if denominator != 0 {
+        Some(numerator / denominator) // successful division
+    } else {
+        None // handle divide-by-zero
+    };
+
+    match result {
+        Some(value) => println!("Result: {}", value),
+        None => println!("Error: Cannot divide by zero!"),
+    }
+}
