e116eef779fc9ccf4ce0feb5c03bdbb2b9c20dcf
adisharma9120 <aditya91sharma73619@gmail.com>
2025-08-29 12:10:38 +0530

    new one.

--- src/secondlect/fib.rs
+++ src/secondlect/fib.rs
@@ -1,4 +1,4 @@
-fn  main(){
+/* fn  main(){
     let n =10;;
     let mut a = 0;
     let mut b = 1;
@@ -12,4 +12,30 @@ fn  main(){
    a=b;
    b= temp;
  }
+} */
+
+fn main()
+{
+    println!("{}", fib(10));
+}
+ fn fib(num:u32) ->u32{
+    let mut first = 0;
+    let mut second = 1;
+
+
+ if num == 0 
+ {
+    return 0
+ }
+ if num ==1
+ {
+    return 1
+ }
+  for _ in 0..(num-2){
+    let temp = second;
+    second= first+second;
+    first = temp;
+
+  }
+  return second;
 }
