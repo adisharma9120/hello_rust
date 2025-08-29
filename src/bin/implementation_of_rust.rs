069c3b467cbcc9327efb2a72b2eca0acfc77ca28
adisharma9120 <aditya91sharma73619@gmail.com>
2025-08-29 17:22:44 +0530

    updated the unuse variable react over here.

--- src/bin/implementation_of_rust.rs
+++ src/bin/implementation_of_rust.rs
@@ -2,32 +2,34 @@ struct Rect {
     width: u32,
     height: u32,
 }
+
 impl Rect {
-    // this is fn implementation
+    // calculate area
     fn area(&self) -> u32 {
-        return self.width * self.height;
+        self.width * self.height
     }
 
+    // calculate perimeter
     fn perimeter(&self) -> u32 {
-        return 2 * (self.width + self.height);
+        2 * (self.width + self.height)
     }
+
+    // double the size of the rectangle
     fn double_size(&mut self) {
         self.width *= 2;
         self.height *= 2;
     }
 }
+
 fn main() {
-    let rect = Rect {
-        width: 32,
-        height: 10,
-    };
     let mut rect = Rect {
         width: 10,
         height: 5,
     };
+
     rect.double_size();
     println!("New width: {}, height: {}", rect.width, rect.height);
 
-    println!("The Area of rectangle is {}", rect.area());
+    println!("The area of the rectangle is {}", rect.area());
     println!("The perimeter of the rectangle is {}", rect.perimeter());
 }
