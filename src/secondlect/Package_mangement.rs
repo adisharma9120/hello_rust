829d2a5d619c90e8284e932c50f185eebf1ab328
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-01 02:44:51 +0530

    add another name in the string.

--- src/secondlect/Package_mangement.rs
+++ src/secondlect/Package_mangement.rs
@@ -1,4 +1,4 @@
-use chrono::Local;
+/* use chrono::Local;
 
 fn main()
 {
@@ -6,4 +6,10 @@ fn main()
  println!("Current time is {}", now);
 
 }
+ */
 
+fn main() {
+    let mut name = String::from("aditya");
+    name.push_str(" sharma");
+    println! {"Full name is: { }", name};
+}
