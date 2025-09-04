8c665dc7e3ee269cff73d3d7d4c4d5a4c8704b0c
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-04 23:33:54 +0530

    Clone exmple.

---
+++ src/secondlect/Clone.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let a1 = String::from("adirya");
+    let mut a2 = a1.clone();
+    a2.push_str("kasie ho tm ");
+    println!("Number is : {}", a1);
+    println!("String after adding something in the string : {}", a2);
+}
