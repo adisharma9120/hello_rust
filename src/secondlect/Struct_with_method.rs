3a30c94d052da41a47a5ea02bd06df9065ff978b
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-05 23:20:50 +0530

    Struct with methods.

---
+++ src/secondlect/Struct_with_method.rs
@@ -0,0 +1,18 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 5,
+        height: 10,
+    };
+    println!("Area = {}", rect.area());
+}
