0229330c8741209fdcefcfa213c698bcfbffda61
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-08 02:19:23 +0530

    ...

--- src/After2/Moving.rs
+++ src/After2/Moving.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     let a1 = String::from("aditya");
     let a2 = take_ownership(a1);
     println!("number is {}", a2);
@@ -6,4 +6,16 @@ fn main() {
 fn take_ownership(s: String) -> String {
     println!("after the ownership is: {}", s);
     s
+}
+ */
+
+fn main() {
+    let a1 = String::from("adithya");
+    {
+        let a2 = a1.clone(); // if u have 2 owner so no worry create a two space for
+                             // storage of data where pointer point the same value
+
+        println!("number is : {}", a2);
+    }
+    println!("Number is : {}", a1);
 }
