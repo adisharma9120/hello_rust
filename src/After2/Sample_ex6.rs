31e826ca5a3c2e3ee4e86ac77cffa69f346da8cb
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-10 22:31:25 +0530

    .

---
+++ src/After2/Sample_ex6.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut cities = HashMap::new();
+
+    cities.insert(String::from("Delhi"), 19000000);
+    cities.insert(String::from("Mumbai"), 21000000);
+
+    let delhi_population = cities.get("Delhi");
+    println!("Population of Delhi: {:?}", delhi_population); // Some(19000000)
+}
