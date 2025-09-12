5a209c2ab380555bfab59b247d719f74c391b022
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-12 12:54:57 +0530

    .

--- src/After2/Hashmap1.rs
+++ src/After2/Hashmap1.rs
@@ -1,17 +1,15 @@
 use std::collections::HashMap;
 
-fn main(){
+fn main() {
     let mut hm: HashMap<&str, i32> = HashMap::new();
-     
 
     // <String, i32> use karte ho to tmko aise declare krna padega
-   // hm.insert(String::from("Aditya"), 33);
+    // hm.insert(String::from("Aditya"), 33);
 
-   // <&str, i32> to u have to do like that
-   //  hm.insert("Aditya", 33);
-   ghit 
+    // <&str, i32> to u have to do like that
+    //  hm.insert("Aditya", 33);
+
     hm.insert("Aditya", 33);
     hm.insert("Laliya", 32);
-     println!("{:?}", hm);
-
+    println!("{:?}", hm);
 }
