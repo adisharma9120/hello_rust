65c49d7c24bfadce2fcb858363f7c7568e6a0fca
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-12 23:44:00 +0530

    .

--- src/After2/Iterator_next.rs
+++ src/After2/Iterator_next.rs
@@ -2,30 +2,9 @@ fn main() {
     let mut v1 = vec![1, 2, 3];
     let mut v1_iter = v1.iter_mut();
 
-    /*    let first_number = v1_iter.next();
-       let second_number = v1_iter.next();
-       let third_number = v1_iter.next();
-    */
-    
+    let first_number = v1_iter.next();
+    let second_number = v1_iter.next();
+    // let third_number = v1_iter.next();
 
     println!("{:?}", v1_iter);
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
