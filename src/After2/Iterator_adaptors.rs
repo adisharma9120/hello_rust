d0e98cd51a70a9e16d9a9a71d91f8989556cd6a9
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-14 02:28:20 +0530

    MAP iterator Aaptor ka use hua jo ki value ko badal de rha hai.

--- src/After2/Iterator_adaptors.rs
+++ src/After2/Iterator_adaptors.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     let v1: Vec<i32> = vec![1, 2, 3];
     let iter = v1.iter();
 
@@ -6,3 +6,22 @@ fn main() {
         println!("{}", n * 2);
     }
 }
+
+
+
+
+v1 = [1,2,3] banaya
+iter = [&1, &2, &3] (iterator banaya jo references deta hai)
+map(|x| x + 1) = har value me +1 add kar diya → [2,3,4]
+for loop chalaya aur values print kar di
+ */
+ fn main()
+ {
+    let v1: Vec<i32> = vec![1,2,3];
+    let iter = v1.iter();
+    let iter2 = iter.map(|x| x + 1);
+
+    for x in iter2{
+        println!("{}", x);
+    }
+ }
