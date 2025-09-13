ea7088c7871485bfd63ad90fd0cea7d0f0165f31
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-14 02:38:44 +0530

    filter iterator adaptor is used.

--- src/After2/Iterator_adaptors.rs
+++ src/After2/Iterator_adaptors.rs
@@ -17,11 +17,12 @@ for loop chalaya aur values print kar di
  */
  fn main()
  {
-    let v1: Vec<i32> = vec![1,2,3];
+    let v1: Vec<i32> = vec![1,2,3,4,5,6,7];
     let iter = v1.iter();
-    let iter2 = iter.map(|x| x + 1);
+        let iter2 = iter.filter(|x| *x % 2==0);
 
     for x in iter2{
+           
         println!("{}", x);
     }
  }
