5ec502ce6348e7e0a44129fa3f3a4822c4fa4cd3
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-14 01:28:05 +0530

    .

--- src/After2/Consuming_Adaptters.rs
+++ src/After2/Consuming_Adaptters.rs
@@ -2,13 +2,13 @@ fn main() {
     let v1 = vec![1, 2, 3];
     let v1_iter = v1.iter();
 
-    //  let v1_iter = v1.into_iter(); //  uye use  ni kr skte hm 
+    //  let v1_iter = v1.into_iter(); //  uye use  ni kr skte hm
     // ownership move ho gaya.
 
     let sum: i32 = v1_iter.sum();
 
     println!("Sum is : {}", sum);
 
-     for i  in v1_iter {}
+    for i in v1_iter {}
     println!("{:?}", v1);
 }
