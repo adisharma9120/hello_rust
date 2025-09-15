7cb26b81d05086f01ade41d0c460f92ae2a3d1f1
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-15 15:44:14 +0530

    .clear() function pura String ko empty kar deta hai (uske andar ke sab characters hata deta hai).
    Matlab ab word ki length 0 ho gayi.
    
    Lekin dhyaan do   iske baad word2 jo slice tha, wo ab invalid ho gaya.
    
     Kyon?
    
    Kyunki word2 ab bhi purane memory location ko refer kar raha hai jo .clear() ne modify/destroy kar diya.
    
    Ye Rust ke rule ke hisaab se dangling reference ho jaata hai.

--- src/After2/slice1.rs
+++ src/After2/slice1.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     let name = String::from("Hello World");
 
     let ans = first_word(name.clone()); // clone kyunki ownership ja rahi hai
@@ -36,4 +36,16 @@ fn last_word(s: String) -> String {
     }
 
     return ans;
+
 }
+ */
+
+ fn main()
+ {
+    let mut word = String::from("hellor world");
+    let word2 = &word[0..5];
+
+    println!("{}", word2);
+    
+    word.clear();
+ }
