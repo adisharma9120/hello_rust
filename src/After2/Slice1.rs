b1c84c6beab5dc3b7fdc975fe0cc16eb9e95d924
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-15 14:59:09 +0530

    .

---
+++ src/After2/Slice1.rs
@@ -0,0 +1,39 @@
+/* fn  main()
+{
+    let name = String::from("Aditya");
+
+    let first_name = &name[0..5];
+    let last_name= &name[5..];
+    println!("{}, {}", first_name, last_name);
+
+}
+
+*/
+ // Write a function that takes string as an input and 
+ //  return the first word from it ......
+/* 
+fn main()
+{
+    let name = String::from("hello world");
+    let ans = first_word(name);
+    println!("ans is: {}", ans);
+}
+ fn first_word(str:String) ->String{
+/* 
+Ye ek function definition hai:
+Parameter: str:String → function ek String lega (ownership ke saath).
+Return type: -> String → function ek String return karega.
+str naam ka variable yaha ek local copy ban jaata hai (jo original name ka ownership le leta hai). */
+    let mut ans = String::from("");
+    for i in str.chars(){ //.chars() ek iterator banata hai jo string ke har character ko ek–ek karke deta hai.
+                    // Example: "hello world".chars() → ['h', 'e', 'l', 'l', 'o', ' ', 'w', ...]
+        if i == ' '{
+          break;
+        }   
+        ans.push_str(&i.to_string());
+     }
+     return ans;
+ }
+
+ */
+ 
--- src/After2/tempCodeRunnerFile.rs
+++ src/After2/tempCodeRunnerFile.rs
@@ -1,3 +1,1 @@
-   let first_number = v1_iter.next();
-    let second_number = v1_iter.next();
-    let third_number = v1_iter.next();
+   
