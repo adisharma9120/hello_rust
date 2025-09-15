8f69607159fb8652fe94d5f7992c9f384050c840
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-15 15:03:03 +0530

    in this code we print the first word ,secon word of the tring.

---
+++ src/After2/slice1.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let name = String::from("Hello World");
+
+    let ans = first_word(name.clone()); // clone kyunki ownership ja rahi hai
+    let ans2 = last_word(name);
+
+    println!("First word: {}", ans);
+    println!("Last word: {}", ans2);
+}
+
+fn first_word(s: String) -> String {
+    let mut ans = String::from("");
+    for i in s.chars() {
+        if i == ' ' {
+            break;
+        }
+        ans.push(i); // push ek char leta hai, to_string() ki zarurat nahi
+    }
+    return ans;
+}
+
+fn last_word(s: String) -> String {
+    let mut ans = String::from("");
+    let mut collect = false;
+
+    for i in s.chars().rev() { // reverse loop
+        if i == ' ' {
+            break;
+        }
+        ans.push(i);
+        collect = true;
+    }
+
+    if collect {
+        return ans.chars().rev().collect(); // kyunki reverse me ulta collect hua
+    }
+
+    return ans;
+}
