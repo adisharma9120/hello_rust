bff48659808d6922a0d8b123b78c54a5777e392e
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-16 02:48:51 +0530

    clippy .

--- src/bin/function.rs
+++ src/bin/function.rs
@@ -1,10 +1,10 @@
-
-fn main(){
+fn main() {
     let a = 20;
     let b = 30;
-    let sum = do_sum(a,b);
+    let sum = do_sum(a, b);
     println!("Sum is: {}", sum);
 }
-fn do_sum(a: i32,b:i32) -> i32{
-  return a+b;
-}    
+
+fn do_sum(a: i32, b: i32) -> i32 {
+    a + b
+}
