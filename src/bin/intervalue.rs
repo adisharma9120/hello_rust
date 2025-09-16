86c8c77c94d7187bea00b0d4c4b4951a9e84c4e1
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-16 11:40:48 +0530

    Due to create an ampty vecotr for the integers.

--- src/bin/intervalue.rs
+++ src/bin/intervalue.rs
@@ -1,8 +1,14 @@
-fn main() {
+/* fn main() {
     let mut x: Vec<i32> = Vec::new(); // create an empty vector for integers
-    
+
     x.push(9);  // push the first number
     x.push(10); // push another number
 
+    println!("{:?}", x); // prints [9, 10]
+}
+ */
+
+fn main() {
+    let x: Vec<i32> = vec![9, 10]; // directly initialize with values
     println!("{:?}", x); // prints [9, 10]
 }
