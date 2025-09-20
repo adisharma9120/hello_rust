cb200cc3b25167601d809a3cc2fc1f5d2bdba307
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-20 19:01:43 +0530

    string literal Rust me staticallly allocated hota hai -> mtlb ye program ke read only memory me store hota hai .

---
+++ src/After2/stringsliterals.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let word = "sadituya";
+}
+
+/* "sadituya" ek string literal hai.
+String literal Rust me statically allocated hota hai → matlab ye program ke read-only memory me store hota hai.
+word ka type hota hai &'static str (ek reference jo hamesha valid rahega jab tak program chal raha hai).
+ */
