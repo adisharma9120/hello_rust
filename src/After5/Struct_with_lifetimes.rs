6db76beee6a6da90a90dd1f8be92e3ec3e425f7b
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-22 01:40:51 +0530

    .

--- src/After5/Struct_with_lifetimes.rs
+++ src/After5/Struct_with_lifetimes.rs
@@ -1,4 +1,4 @@
-struct User<'a> {
+/* struct User<'a> {
     name: &'a str,
 }
 
@@ -7,3 +7,26 @@ fn main() {
     let user = User { name: &name };
     println!("The name of the user is :{}", user.name);
 }
+ */
+
+struct User<'a> {
+    name: &'a str,
+}
+
+fn main() {
+    let user;
+    {
+        let name = String::from("Aditya");
+        let user = User { name: &name }; // ❌ `name` dies here
+    }
+    // println!("The name of the user is :{}", user.name);
+}
+
+
+U/* ser<'a> says: “I contain a reference &'a str that must live at least as long as 'a.”
+
+But name is created inside the inner block and dropped at the end of that block.
+
+That makes user.name point to invalid memory → Rust prevents this.
+
+So this is not about the let user; line only — it’s about lifetimes not lining up. */
