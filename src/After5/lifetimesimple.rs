ef6d571ef55ae2492945662344c8d29c3c7270d0
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-22 00:54:05 +0530

    esme lige time ka use kr rhe hai .

--- src/After5/lifetimesimple.rs
+++ src/After5/lifetimesimple.rs
@@ -5,13 +5,10 @@ fn main() {
     let a = String::from("aditya");
     let b = String::from("sharma");
 
-    println!("largest string is :{}", largest_string(a,b));
+    println!("largest string is :{}", largest_string(&a, &b));
+  //     println!("largest string is: {}", largest_string(a, b)); 
 }
-
-fn largest_string(a: String, b: String) -> String {
-    if a.len() > b.len() {
-        return a;
-    } else {
-        return b;
-    }
+// fn largest_string(a: &String, b: &String) -> &String {
+fn largest_string<'a>(a: &'a String, b: &'a String) -> &'a String {
+    if a.len() > b.len() { a } else { b }
 }
