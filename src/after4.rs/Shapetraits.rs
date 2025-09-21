6f8aace181cac2ee43734d9401a417c4ea9c597f
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-21 15:40:24 +0530

    traits are used to implement in the find the area of reactangle.

--- src/after4.rs/Shapetraits.rs
+++ src/after4.rs/Shapetraits.rs
@@ -1,4 +1,4 @@
-trait Shape {
+/* trait Shape {
     fn area(&self) -> f64;
 }
 
@@ -20,4 +20,29 @@ fn main() {
     let circle = Circle { radius: 5.0 };
 
     print_area(&circle);
+}
+ */
+
+trait Shape {
+    fn area(&self) -> f64;
+}
+struct Rectangle {
+    width: f64,
+    heigth: f64,
+}
+impl Shape for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.heigth
+    }
+}
+fn print_area(shape: &impl Shape) {
+    println!("Area is :{}", shape.area());
+}
+
+fn main() {
+    let rectangle = Rectangle {
+        width: 4.0,
+        heigth: 4.0,
+    };
+    print_area(&rectangle);
 }
