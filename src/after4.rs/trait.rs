3e4efdb1ae035ce086715649ceeee15fdffa54c7
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-21 15:27:57 +0530

    .

--- src/after4.rs/trait.rs
+++ src/after4.rs/trait.rs
@@ -1,34 +1,38 @@
 trait Speak {
     fn speak(&self) -> String;
 }
+
 struct Dog {
     name: String,
 }
+
 struct Cat {
     name: String,
 }
 
+// Trait implement for Dog
 impl Speak for Dog {
     fn speak(&self) -> String {
-        format!("woof i am {}", self.name)
+        format!("Woof! I am {}", self.name)
     }
-}
-impl Speak for Cat {
-    fn speak(&self) -> String {
-        format!("Meow! i am {}", self.name)
-    }
-}
-fn animal_speak(animal: &impl Speak) {
-    println!("{}", animal.speak())
 }
 
+// Trait implement for Cat
+impl Speak for Cat {
+    fn speak(&self) -> String {
+        format!("Meow! I am {}", self.name)
+    }
+}
+
+// Function jo kisi bhi Speak trait ko accept kare
+fn animal_speak(animal: &impl Speak) {
+    println!("{}", animal.speak());
+}
+
+// Main function
 fn main() {
-    let dog = Dog {
-        name: String::from("lucy"),
-    };
-    let cat = Cat {
-        name: String::from("tomy"),
-    };
+    let dog = Dog { name: String::from("Lucy") };
+    let cat = Cat { name: String::from("Tomy") };
 
     animal_speak(&dog);
     animal_speak(&cat);
