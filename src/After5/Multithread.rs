e179cc02cf4aa984ddbd7f2088c445df4b70db87
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-22 16:34:35 +0530

    esme hm hndli jio ka use es liye krte hai ki ye asurity deta hia ki main thread ke baaad helper thread bhi print hoga ,,The handle is used to ensure the spawned thread completes..

---
+++ src/After5/Multithread.rs
@@ -0,0 +1,14 @@
+use std::thread;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        // thread::spawn starts a new OS thread.
+        for i in 0..5 {
+            println! {"Hi from spawn thread {}", i};
+        }
+    });
+    for i in 0..50 {
+        println! {"Hi from main thread {}", i};
+    }
+    handle.join();
+}
