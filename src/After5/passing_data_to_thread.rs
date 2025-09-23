5f10c27e94caf4243505a2d653822e91378f6235
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-23 23:51:27 +0530

    Multiple Threads.

--- src/After5/passing_data_to_thread.rs
+++ src/After5/passing_data_to_thread.rs
@@ -1,4 +1,4 @@
-use std::thread;
+/* use std::thread;
 
 fn main() {
     let numbers = vec![1, 2, 3];
@@ -11,3 +11,24 @@ fn main() {
 
     handle.join().unwrap();
 }
+ */
+
+
+ // Multiple Threads
+
+ use std::thread;
+
+fn main() {
+    let t1 = thread::spawn(|| {
+        println!("Thread 1 is running");
+    });
+
+    let t2 = thread::spawn(|| {
+        println!("Thread 2 is running");
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+}
+
+
