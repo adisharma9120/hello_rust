97dbff398b2d9a09d55e7603e65dc1fe5b8e4564
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-25 22:38:11 +0530

    .

--- src/After2/an1.rs
+++ src/After2/an1.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     let mut numbers = Vec::new();
     numbers.push(1);
     numbers.push(2);
@@ -12,5 +12,28 @@ fn main() {
 
     for n in &numbers {
         println!("Remaining: {}", n);
+    }
+}
+
+
+ */
+fn main() {
+    println!("hello world");
+    let name = "Adiii";
+    let age = 22;
+    let weight = 43.4;
+
+    println!("Name : {}", name);
+    println!("age : {}", age);
+    println!("weight :{}", weight);
+
+    let sum = 4 + 10;
+    println!("{}", sum);
+
+    // Conditional example
+    if weight > 70.0 {
+        println!("You are above 70 kg.");
+    } else {
+        println!("You are 70 kg or less.");
     }
 }
