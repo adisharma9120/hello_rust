edc5fea3a8e4e91af5396f89934a75bc0dd81861
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-25 22:47:44 +0530

    adding the pub before the fn main().

--- src/bin2/dangling_doublefreeerror.rs
+++ src/bin2/dangling_doublefreeerror.rs
@@ -1,11 +1,11 @@
-fn main ()
+ pub fn main ()
 {
     create_str();
     //garbag collector
     print!("{}",23);
 }
 
-fn create_str()
+ pub fn create_str()
 {
 let s1 = String::from("hi there");
     print!("{}",s1);
