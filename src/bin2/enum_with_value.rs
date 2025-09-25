2ddd819a57e249dd34d387db87381a31bae5ca72
adisharma9120 <aditya91sharma73619@gmail.com>
2025-09-25 23:06:30 +0530

    Change the fn .

--- src/bin2/enum_with_value.rs
+++ src/bin2/enum_with_value.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 enum Shape {
     Circle(f64),
-    Square(f64),
+
     Rectangle(f64, f64),
 }
 
@@ -9,16 +9,16 @@ enum Shape {
 fn calculate_area(shape: Shape) -> f64 {
     match shape {
         Shape::Circle(r) => std::f64::consts::PI * r * r,
-        Shape::Square(s) => s * s,
+
         Shape::Rectangle(w, h) => w * h,
     }
 }
 fn main() {
     let circle = Shape::Circle(5.0);
-    let square = Shape::Square(4.0);
+
     let rectangle = Shape::Rectangle(3.0, 6.0);
 
     println!("Circle area: {}", calculate_area(circle));
-    println!("Square area: {}", calculate_area(square));
+
     println!("Rectangle area: {}", calculate_area(rectangle));
 }
